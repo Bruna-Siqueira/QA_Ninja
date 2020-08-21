349321b574bd6ab93b6f8feeddc7e8117fc802ed
Bruna Siqueira <siqueira.bruna@outlook.com>
2020-08-20 22:47:00 -0300

    programacao basica em ruby

---
+++ ruby/basico/tipos.rb
@@ -0,0 +1,16 @@
+# O tipo de uma variável é definido no momento em que a mesma recebe o valor
+
+produto = 'Mackbook Air'
+preco = 6000.00
+quantidade = 10
+disponivel = true
+
+puts produto
+puts preco
+puts quantidade
+puts disponivel
+
+puts produto.class
+puts preco.class
+puts quantidade.class
+puts disponivel.class
