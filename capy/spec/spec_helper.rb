c70faf96b27e4f2b5b001cd7dd5288778d4daf98
Bruna Siqueira <siqueira.bruna@outlook.com>
2020-09-02 17:21:33 -0300

    obtendo screenshots

--- capy/spec/spec_helper.rb
+++ capy/spec/spec_helper.rb
@@ -15,6 +15,12 @@ RSpec.configure do |config|
   config.before(:example) do
     page.current_window.resize_to(1200, 800)
   end
+
+  config.after(:example) do |e|
+    # Regex que substitui caracter especial por vazio e espaço em branco por underline
+    nome = e.description.gsub(/[^A-Za-z0-9 ]/, '').tr(' ', '_')
+    page.save_screenshot("log/" + nome + ".png")
+  end
 end
 
 Capybara.configure do |config|
